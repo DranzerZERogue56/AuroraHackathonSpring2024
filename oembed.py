8e96c6069b23f0a3c75259b7447337b1d094bc2c
aschneider15 <69209408+aschneider15@users.noreply.github.com>
2024-04-16 18:17:04 -0500

    Initial code for oembed created.
    "Never gonna give you up" by Rick Astley is the example. It provides, in JSON format, the title and author of the content, along with a bunch of useless crap like aspect ratio.

---
+++ oembed.py
@@ -0,0 +1,11 @@
+# make sure you execute "pip install pyoembed." Recommend doing this as admin
+
+from pyoembed import oEmbed, PyOembedException
+
+try:
+
+    data = oEmbed('https://youtu.be/dQw4w9WgXcQ', maxwidth=640, maxheight=480)
+    print(data)
+
+except:
+    print("Error: unable to parse provided URL.")
